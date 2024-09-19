926a6c6b4c3f24e10c33b109e5a469be97e9b2c4
userDipanshu <dipanshu@mrmprocom.com>
2024-09-19 12:20:15 +0530

    changes to tell dnd which elements are draggables

--- src/components/draggable_section/Droppable.jsx
+++ src/components/draggable_section/Droppable.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Draggable from "./draggable/Draggable";
 import { closestCorners, DndContext } from "@dnd-kit/core";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 import "./dropable.scss";
 
 export default function Droppable({ array, max }) {
   return (
     <DndContext collisionDetection={closestCorners}>
       <div className="droppable">
-        {array.map((algo, index) => {
-          return (
-            <React.Fragment key={index + " " + algo}>
-              <Draggable algo={algo} max={max} />
-            </React.Fragment>
-          );
-        })}
+        <SortableContext items={array} strategy={verticalListSortingStrategy}>
+          {array.map((algo, index) => {
+            return (
+              <React.Fragment key={index + " " + algo}>
+                <Draggable algo={algo} max={max} />
+              </React.Fragment>
+            );
+          })}
+        </SortableContext>
       </div>
     </DndContext>
   );
