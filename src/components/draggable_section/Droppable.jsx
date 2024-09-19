10a574fe1cdfed53fe109d7bf43d357b806f45b7
userDipanshu <dipanshu@mrmprocom.com>
2024-09-19 12:11:33 +0530

    imported the dnd context to wrap draggable and droppable area

--- src/components/draggable_section/Droppable.jsx
+++ src/components/draggable_section/Droppable.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import Draggable from "./draggable/Draggable";
+import { closestCorners, DndContext } from "@dnd-kit/core";
 import "./dropable.scss";
 
 export default function Droppable({ array, max }) {
   return (
-    <div className="droppable">
-      {array.map((algo, index) => {
-        return (
-          <React.Fragment key={index + " " + algo}>
-            <Draggable
-              algo={algo}
-              max={max}
-            />
-          </React.Fragment>
-        );
-      })}
-    </div>
+    <DndContext collisionDetection={closestCorners}>
+      <div className="droppable">
+        {array.map((algo, index) => {
+          return (
+            <React.Fragment key={index + " " + algo}>
+              <Draggable algo={algo} max={max} />
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </DndContext>
   );
 }
